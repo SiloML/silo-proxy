0774d60b9a7dfd13dfa8b0eae25e283e28f9d3c0
dhruvik7 <dhruvik.parikh@hotmail.com>
2020-01-06 15:14:02 -0800

    added forwarding and queueing

--- mytornado.py
+++ mytornado.py
@@ -4,6 +4,7 @@ import tornado.websocket
 
 class MainHandler(tornado.websocket.WebSocketHandler):
     waiters = []
+    check = 123
     def open(self):
         MainHandler.waiters.append(self)
         print(self)
@@ -15,42 +16,56 @@ class MainHandler(tornado.websocket.WebSocketHandler):
         for waiter in MainHandler.waiters:
             waiter.write_message("someone said ", msg)
 
-class AcceptOwnerHandler(tornado.websocket.WebSocketHandler):
-    async def open(self):
-        #define the url using some firebase call
-        conn = await websocket_connect(url)
-        print("hello")
-        async def proxy_loop():
-            while True:
-                msg = await conn.read_message()
-                if msg is None:
-                    break
-                print(msg)
-                await self.write_message(msg)
-        ioloop.IOLoop.current().spawn_callback(proxy_loop)
-    async def on_close(self):
-        return
-    async def on_message(self, msg):
-        print("forwarding message")
+class OwnerHandler(tornado.websocket.WebSocketHandler):
+    datasets = {}
+    n = 0
 
-class AuthorizeUserHandler(tornado.websocket.WebSocketHandler):
-    async def open(self):
-        #define the url using some firebase call
-        print("connected")
-        #conn = await websocket_connect("http://54.183.80.47:10000")
-        async def proxy_loop():
-            while True:
-                msg = await conn.read_message()
-                if msg is None:
-                    break
-                print("msg:", msg)
-                await self.write_message(msg)
-        ioloop.IOLoop.current().spawn_callback(proxy_loop)
+    def open(self):
+        #verify that the data owner has connected with a valid token
+        cookie = self.request.headers["cookie"]
+        if cookie is not None:
+            print("firebase call to verify that the cookie is valid:" + cookie)
+            OwnerHandler.datasets[OwnerHandler.n] = self
+            OwnerHandler.n += 1
+            print("firebase call to update the list of available servers")
+
+    def on_close(self):
+        print("an owner has disconnected, need to let firebase know")
+        OwnerHandler.datasets = {key: val for key, val in OwnerHandler.items() if val != self}
+
+    def on_message(self, msg):
+        print("looking at which researcher is currently connected to this owner and forwarding them the message")
+        if len(ResearcherHandler.resMap[self]) > 0:
+            ResearcherHandler.resMap[self][0].write_message(msg)
+
+class ResearcherHandler(tornado.websocket.WebSocketHandler):
+    dest = 0
+    resMap = {}
+    def open(self):
+        #verify that the researcher has connected with a valid token
+        cookie = self.request.headers["cookie"]
+        if cookie is not None:
+            print("firebase call to verify that the researcher cookie is valid:" + cookie) #also removes the token and returns the database id
+            self.dest = 0
+            if dest not in ResearcherHandler.resMap.keys():
+                ResearcherHandler.resMap[OwnerHandler.datasets[self.dest]] = [self]
+            else:
+                ResearcherHandler.resMap[OwnerHandler.datasets[self.dest]].append(self)
+
+    def on_close(self):
+        ResearcherHandler.resMap[OwnerHandler.datasets[self.dest]].remove(self)
+
+    def on_message(self, msg):
+        #only send the message if they are first in line, otherwise, wait
+        while(ResarcherHandler.resMap[OwnerHandler.datasets[self.dest]][0] is not self):
+            sleep(1)
+        OwnerHandler.datasets[self.dest].write_message(msg)
+        
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/AcceptOwner", MainHandler),
-        (r"/AuthorizeUser", AuthorizeUserHandler)
+        (r"/share", OwnerHandler),
+        (r"/research", ResarcherHandler)
     ])
 def check_origin(self, data):
     return True
