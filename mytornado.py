c5286bdfd34bcc61ce6166c6676af232e31c7846
dhruvik7 <dhruvik.parikh@hotmail.com>
2020-01-06 17:26:34 -0800

    fixing queue cleanup

--- mytornado.py
+++ mytornado.py
@@ -48,12 +48,16 @@ class ResearcherHandler(tornado.websocket.WebSocketHandler):
         cookie = self.request.headers["cookie"]
         if cookie is not None:
             print("firebase call to verify that the researcher cookie is valid:" + cookie) #also removes the token and returns the database id
+            self.dest = 125
             await OwnerHandler.datasets[self.dest][1].put(self)
             ResearcherHandler.resMap[OwnerHandler.datasets[self.dest][0]] = self
 
-    def on_close(self):
+    async def on_close(self):
+        print("researcher closing")
         if (ResearcherHandler.resMap[OwnerHandler.datasets[self.dest][0]] == self):
-            OwnerHandler.datasets[self.dest][1].task_done()
+            print("removing")
+            async for item in OwnerHandler.datasets[self.dest][1]:
+                await OwnerHandler.datasets[self.dest][1].task_done()
 
     def on_message(self, msg):
         #only send the message if they are first in line
